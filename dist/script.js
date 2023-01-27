368071ce84a9de68d57f99b55877ac65715d7c3c
MartinkaOff <110162487+MartinkaOff@users.noreply.github.com>
2023-01-27 16:32:29 +0600

    Services section and add Slider for this section. Modified site, created common styles.

--- dist/script.js
+++ dist/script.js
@@ -163,13 +163,21 @@ const slider = () => {
     "speed": 400,
     "nav": false
   });
-  let sliderGuarantee = tns({
-    "container": '.guarantee__slider',
-    "controlsContainer": ".slider__controls-guarantee",
+  let sliderBlock = tns({
+    "container": '.sliderBlock',
+    "controlsContainer": ".slider-controls",
     "items": 1,
-    "swipeAngle": false,
-    "speed": 400,
-    "mouseDrag": true
+    "speed": 1000,
+    "mouseDrag": true,
+    "nav": false
+  });
+  let slideServices = tns({
+    "container": '.services__slider',
+    "controlsContainer": ".slider-controls-services",
+    "items": 1,
+    "speed": 1000,
+    "mouseDrag": true,
+    "nav": false
   });
 };
 
--- src/js/slider.js
+++ src/js/slider.js
@@ -10,13 +10,21 @@ const slider = () => {
         "speed": 400,
         "nav": false
     });
-    let sliderGuarantee = tns({
-        "container": '.guarantee__slider',
-        "controlsContainer": ".slider__controls-guarantee",
+    let sliderBlock = tns({
+        "container": '.sliderBlock',
+        "controlsContainer": ".slider-controls",
         "items": 1,
-        "swipeAngle": false,
-        "speed": 400,
+        "speed": 1000,
         "mouseDrag": true,
+        "nav": false
+    });
+    let slideServices = tns({
+        "container": '.services__slider',
+        "controlsContainer": ".slider-controls-services",
+        "items": 1,
+        "speed": 1000,
+        "mouseDrag": true,
+        "nav": false
     });
 }
 export default slider;
